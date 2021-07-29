b69930373313d5e6bb7d6271081bf12f386d39ee
Roman <sholokh.r@gmail.com>
2021-07-29 17:32:40 +0300

    Initial commit

---
+++ src/main/java/com/romansholokh/openstreetmap/springbootclient/OpenStreetMapSpringBootClientApplication.java
@@ -0,0 +1,13 @@
+package com.romansholokh.openstreetmap.springbootclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenStreetMapSpringBootClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenStreetMapSpringBootClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romansholokh/openstreetmap/springbootclient/OpenStreetMapSpringBootClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romansholokh.openstreetmap.springbootclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenStreetMapSpringBootClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
