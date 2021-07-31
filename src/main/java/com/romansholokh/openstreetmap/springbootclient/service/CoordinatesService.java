9bd0a1fc23449c77e9ac6db9dd8e2e7ec3fb73a2
Roman <sholokh.r@gmail.com>
2021-07-31 19:05:25 +0300

    Added add() method to CoordinatesService

--- src/main/java/com/romansholokh/openstreetmap/springbootclient/service/CoordinatesService.java
+++ src/main/java/com/romansholokh/openstreetmap/springbootclient/service/CoordinatesService.java
@@ -1,9 +1,19 @@
 package com.romansholokh.openstreetmap.springbootclient.service;
 
+import com.romansholokh.openstreetmap.springbootclient.entity.Coordinates;
+import com.romansholokh.openstreetmap.springbootclient.repository.CoordinatesRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class CoordinatesService {
+
+    private final CoordinatesRepository coordinatesRepository;
+
+    public Coordinates add(Coordinates coordinates) {
+        return coordinatesRepository.save(coordinates);
+    }
 }
