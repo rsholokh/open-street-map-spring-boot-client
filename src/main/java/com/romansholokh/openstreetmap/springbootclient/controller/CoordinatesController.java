2747a03bb945cf9fc0583174aafddd158ee5efea
Roman <sholokh.r@gmail.com>
2021-07-31 19:03:31 +0300

    Added empty getCoordinatesFromAddress() method to CoordinatesController

--- src/main/java/com/romansholokh/openstreetmap/springbootclient/controller/CoordinatesController.java
+++ src/main/java/com/romansholokh/openstreetmap/springbootclient/controller/CoordinatesController.java
@@ -1,9 +1,23 @@
 package com.romansholokh.openstreetmap.springbootclient.controller;
 
+import com.romansholokh.openstreetmap.springbootclient.pojo.Address;
+import com.romansholokh.openstreetmap.springbootclient.service.CoordinatesService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@AllArgsConstructor
 public class CoordinatesController {
+
+    private final CoordinatesService coordinatesService;
+
+    @GetMapping("/getCoordinates")
+    public ResponseEntity getCoordinatesFromAddress (@RequestBody Address address) {
+
+    }
 }
