206484b1335e375c0084cb8f807deba2fcb2d0f8
Roman <sholokh.r@gmail.com>
2021-08-03 11:56:32 +0300

    CoordinatesController: added getCoordinatesFromAddressAndAddCoordinatesToDB() method with tests

--- src/main/java/com/romansholokh/openstreetmap/springbootclient/controller/CoordinatesController.java
+++ src/main/java/com/romansholokh/openstreetmap/springbootclient/controller/CoordinatesController.java
@@ -1,13 +1,18 @@
 package com.romansholokh.openstreetmap.springbootclient.controller;
 
+import com.romansholokh.openstreetmap.springbootclient.entity.Coordinates;
 import com.romansholokh.openstreetmap.springbootclient.pojo.Address;
 import com.romansholokh.openstreetmap.springbootclient.service.CoordinatesService;
+import com.romansholokh.openstreetmap.springbootclient.util.jsonparser.JsonParser;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/")
@@ -16,8 +21,33 @@ public class CoordinatesController {
 
     private final CoordinatesService coordinatesService;
 
-    @GetMapping("/getCoordinates")
-    public ResponseEntity getCoordinatesFromAddress (@RequestBody Address address) {
-        return null;
+    @PostMapping("/getCoordinates")
+    public ResponseEntity<Coordinates> getCoordinatesFromAddressAndAddCoordinatesToDB(@RequestBody Address address) {
+        String city = address.getCity();
+        String street = address.getStreet();
+        String building = address.getBuilding();
+
+        String url = "https://nominatim.openstreetmap.org/search/{city}%20{street}%20{building}?format=json&limit=1";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String coordinatesJson = null;
+        try {
+            coordinatesJson = restTemplate.getForObject(url, String.class, city, street, building);
+        } catch (RestClientException e) {
+            e.printStackTrace();
+        }
+
+        assert coordinatesJson != null;
+        Coordinates coordinates = null;
+        try {
+            coordinates = JsonParser.parseJsonToCoordinatesObject(coordinatesJson);
+        } catch (Exception e) {
+            return new ResponseEntity("Oops, something went wrong. " +
+                    "Unable to parse response from the api. " +
+                    "Maybe the Address you entered was incorrect.", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(coordinatesService.add(coordinates));
     }
 }
---
+++ src/test/java/com/romansholokh/openstreetmap/springbootclient/controller/CoordinatesControllerTest.java
@@ -0,0 +1,60 @@
+package com.romansholokh.openstreetmap.springbootclient.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.romansholokh.openstreetmap.springbootclient.pojo.Address;
+import com.romansholokh.openstreetmap.springbootclient.service.CoordinatesService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = CoordinatesController.class)
+class CoordinatesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CoordinatesService coordinatesService;
+
+    @Test
+    void whenValidInput_thenReturns200() throws Exception {
+        Address address = new Address("London", "Piccadilly", "Burlington House");
+
+        mockMvc.perform(post("/getCoordinates")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(address)))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void whenGetJsonFromUrl_thenStatus200() {
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        String city = "London";
+        String street = "Piccadilly";
+        String building = "Burlington House";
+
+        String url = "https://nominatim.openstreetmap.org/search/{city}%20{street}%20{building}?format=json&limit=1";
+
+        String givenJson = "[{\"place_id\":98690364,\"licence\":\"Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright\",\"osm_type\":\"way\",\"osm_id\":31510288,\"boundingbox\":[\"51.508883\",\"51.5096454\",\"-0.1403344\",\"-0.1390605\"],\"lat\":\"51.5092768\",\"lon\":\"-0.13973813543557267\",\"display_name\":\"Royal Academy of Arts, Piccadilly, St. James's, City of Westminster, London, Greater London, England, W1J 0BD, United Kingdom\",\"class\":\"tourism\",\"type\":\"museum\",\"importance\":0.7644724074563636,\"icon\":\"https://nominatim.openstreetmap.org/ui/mapicons//tourist_museum.p.20.png\"}]";
+        String expectedJson = restTemplate.getForObject(url, String.class, city, street, building);
+
+        System.out.println(expectedJson);
+
+        assertThat(expectedJson, is(givenJson));
+    }
+
+
+}
