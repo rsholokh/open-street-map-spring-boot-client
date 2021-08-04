d4e50baf260d0b037888fcda1b1fddf3777e6423
Roman <sholokh.r@gmail.com>
2021-08-04 21:03:27 +0300

    JsonParser: added parseJsonToAddressObject() method & test for it

--- src/main/java/com/romansholokh/openstreetmap/springbootclient/util/jsonparser/JsonParser.java
+++ src/main/java/com/romansholokh/openstreetmap/springbootclient/util/jsonparser/JsonParser.java
@@ -3,6 +3,7 @@ package com.romansholokh.openstreetmap.springbootclient.util.jsonparser;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.romansholokh.openstreetmap.springbootclient.entity.Coordinates;
+import com.romansholokh.openstreetmap.springbootclient.pojo.Address;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -15,5 +16,11 @@ public class JsonParser {
         List<Coordinates> list = gson.fromJson(json, type);
 
         return list.get(0);
+    }
+
+    public static Address parseJsonToAddressObject(String json) throws Exception {
+        Gson gson = new Gson();
+
+        return gson.fromJson(json, Address.class);
     }
 }
--- src/test/java/com/romansholokh/openstreetmap/springbootclient/util/jsonparser/JsonParserTest.java
+++ src/test/java/com/romansholokh/openstreetmap/springbootclient/util/jsonparser/JsonParserTest.java
@@ -1,6 +1,7 @@
 package com.romansholokh.openstreetmap.springbootclient.util.jsonparser;
 
 import com.romansholokh.openstreetmap.springbootclient.entity.Coordinates;
+import com.romansholokh.openstreetmap.springbootclient.pojo.Address;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,5 +19,16 @@ class JsonParserTest {
 
         assertEquals(expectedLat, coordinates.getLat(), "Latitude does not match!");
         assertEquals(expectedLon, coordinates.getLon(), "Longitude does not match!");
+    }
+
+    @Test
+    void testParseJsonToAddressObject() throws Exception {
+        String json = "{\"place_id\":98690364,\"licence\":\"Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright\",\"osm_type\":\"way\",\"osm_id\":31510288,\"lat\":\"51.5092768\",\"lon\":\"-0.13973813543557267\",\"display_name\":\"Royal Academy of Arts, Piccadilly, St. James's, City of Westminster, London, Greater London, England, W1J 0BD, United Kingdom\",\"address\":{\"tourism\":\"Royal Academy of Arts\",\"road\":\"Piccadilly\",\"neighbourhood\":\"St. James's\",\"city\":\"City of Westminster\",\"state_district\":\"Greater London\",\"state\":\"England\",\"postcode\":\"W1J 0BD\",\"country\":\"United Kingdom\",\"country_code\":\"gb\"},\"boundingbox\":[\"51.508883\",\"51.5096454\",\"-0.1403344\",\"-0.1390605\"]}";
+        String wrongJson = "{\"place_id\":9869://osm.org/copyright\",\"osm_type\":\"way\",\"osm_id\":31510288,\"lat\":\"51.5092768\",\"lon\":\"-0.13973813543557267\",\"display_name\":\"Royal Academy of Arts, Piccadilly, S883\",\"51.5096454\",\"-0.1403344\",\"-0.1390605\"]}";
+        String expectedFullAddress = "Royal Academy of Arts, Piccadilly, St. James's, City of Westminster, London, Greater London, England, W1J 0BD, United Kingdom";
+
+        Address address = JsonParser.parseJsonToAddressObject(json);
+
+        assertEquals(expectedFullAddress, address.getFullAddress(), "Full address does not match!");
     }
 }
